517646762e47d4a47c50a6fcbc8e13095b5dfcfc
Luca Scalzotto <luca@scorpiac.com>
2022-03-12 00:53:30 +0100

    Simple CLI

--- cmd/explainshell.go
+++ cmd/explainshell.go
@@ -5,18 +5,72 @@ import (
 	"explainshell-shell/internal/esweb"
 	"fmt"
 	"os"
+	"strings"
 )
 
 const SEPARATOR = "──────────"
 
+const HELP_TEXT = `
+Usage: {{filename}} [options...] <command>
+
+Options:
+  --            Stop parsing options and interpret the rest as command input.
+  -h, --help    Print this help text.
+
+Example usage:
+  {{filename}} tar xzvf archive.tar.gz
+`
+
 func main() {
-	help, err := esweb.GetCommandHelp("ssh -i keyfile -f -N -L 1234:www.google.com:80 host")
+	argsI := 1
+	for ; argsI < len(os.Args); argsI++ {
+		arg := os.Args[argsI]
+
+		if len(arg) == 0 || arg == "--" {
+			argsI++
+			break
+		}
+
+		if arg[0] != '-' {
+			break
+		}
+
+		switch os.Args[argsI] {
+		case "-h":
+			fallthrough
+		case "--help":
+			printHelp()
+			return
+		default:
+			fmt.Println("Invalid option: " + arg)
+			printHelp()
+			os.Exit(1)
+		}
+	}
+
+	input := ""
+	for ; argsI < len(os.Args); argsI++ {
+		input += os.Args[argsI] + " "
+	}
+	input = strings.TrimSpace(input)
+
+	help, err := esweb.GetCommandHelp(input)
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		os.Exit(2)
 	}
 
 	printCommandHelp(help)
+}
+
+func printHelp() {
+	fileName := os.Args[0]
+	fileName = fileName[strings.LastIndexByte(fileName, '/')+1:]
+
+	help := strings.TrimSpace(HELP_TEXT)
+	help = strings.ReplaceAll(help, "{{filename}}", fileName)
+
+	fmt.Println(help)
 }
 
 func printCommandHelp(help *esweb.CommandHelp) {
